53821ffed9713a4f45227237ba144b90b18e208f
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-15 12:19:22 +0300

    New statpage.js

---
+++ html/statpage.js
@@ -0,0 +1,44 @@
+/*
+ * This page programs elements defined in statpage.pug
+ */
+
+/**
+ * Side panel containing filters.
+ */
+var filterpane = {
+
+    /**
+     * Date pattern dropdown list.
+     *
+     * Defined after initialization.
+     */
+    dateDropdown: undefined,
+
+    /**
+     * Input element for date to filter entries from.
+     *
+     * Defined after initialization.
+     */
+    dateFrom: undefined,
+
+    /**
+     * Input element for date to filter elements to.
+     *
+     * Defined after initialization.
+     */
+    dateTo: undefined,
+
+    /**
+     * Element to select people that are included to statistics.
+     *
+     * Defined after initialization.
+     */
+    people: undefined,
+};
+
+/**
+ * Function to initialize the structure.
+ */
+filterpane.init = function() {
+    console.log("Unimplemented yet");
+};
