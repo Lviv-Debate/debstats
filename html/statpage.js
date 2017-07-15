920e8dbf198ce36d8bcef2cdfbd5914033c204ca
Maxym Naumchyk <max.naumch@gmail.com>
2017-07-15 12:31:42 +0300

    Copied functions that are related to filter side panel

--- html/statpage.js
+++ html/statpage.js
@@ -42,3 +42,120 @@ var filterpane = {
 filterpane.init = function() {
     console.log("Unimplemented yet");
 };
+
+/**
+ * Show date pattern dropdown. The page becomes covered with transparent
+ * layer that listens to click events and hides the dropdown when page gets
+ * clicked. But dropdown remains uncovered.
+ */
+filterpane.showDateDropdown = function() {
+    this.dateDropdown.style.display = "block";
+
+    // Hide dropdown when it looses it's focus.
+    doccover.activate(function() {
+        hideDateDropdown();
+    });
+};
+
+/**
+ * Hide the date pattern dropdown and uncover the page.
+ */
+filterpane.hideDateDropdown = function() {
+    this.dateDropdown.style.display = "none";
+
+    // Uncover the page if it is covered.
+    doccover.reset();
+};
+
+/**
+ * Set given date filter to given value.
+ *
+ * @param obj An "input" value of type 'date'.
+ * @param d Set to given day.
+ * if this parameter is the only one passed to function, it is expected
+ * to be any of these strings:
+ * * "today"        - set to current day.
+ * * "yearago"      - set to date year ago.
+ * * "monthago"     - set to date month ago.
+ * * "weekago"      - set to date 7 days ago.
+ * * "yesterday"    - set to yesterday.
+ * * "monday"       - set to last monday.
+ *
+ * @param m Set to given month.
+ * @param y Set to given year.
+ */
+filterpane.setDate = function(obj, d, m, y) {
+    var date = new Date();
+
+    if (m === undefined) {
+        switch (d) {
+            case "today":
+                date = new Date();
+                break;
+
+            case "yearago":
+                date.setFullYear(date.getFullYear() - 1);
+                break;
+
+            case "monthago":
+                date.setMonth(date.getMonth() - 1);
+                break;
+
+            case "weekago":
+                date.setDate(date.getDate() - 7);
+                break;
+
+            case "yesterday":
+                date.setDate(date.getDate() - 1);
+                break;
+
+            case "monday":
+                var day = date.getDay() - 1;
+                day = day == -1 ? 6 : day;
+                date.setDate(date.getDate() - day);
+                break;
+
+            default:
+                console.log({
+                    msg : "Argument error",
+                    d   : d,
+                    m   : m,
+                    y   : y
+                });
+                return;
+        }
+    } else {
+        // Create Date instance with given values.
+        // Note that month count from 0 in Date implementation whereas
+        // 'm' argument counts from 1.
+        date = new Date(Date.UTC(y, m - 1, d));
+    }
+
+    obj.value = date.yyyymmdd();
+};
+
+Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
+
+    return [
+            this.getFullYear(),
+            (mm > 9 ? '' : '0') + mm,
+            (dd > 9 ? '' : '0') + dd
+    ].join('-');
+};
+
+filterpane.setDateFrom = function(d, m, y) {
+    return setDate(filterDateFrom, d, m, y);
+};
+
+filterpane.setDateTo = function(d, m, y) {
+    return setDate(filterDateTo, d, m, y);
+};
+
+/**
+ * Set description to the people filter element and people count.
+ */
+filterpane.setFilterPeople = function(desc, count) {
+    filterPeople.innerHTML = desc + ' (' + count + ')';
+};
