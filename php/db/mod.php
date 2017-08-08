3bea232b0cd11c3071f9a8895034d009a29b5507
Maxym Naumchyk <max.naumch@gmail.com>
2017-08-08 15:26:16 +0300

    Wraped fns in new FilterData class

--- php/db/mod.php
+++ php/db/mod.php
@@ -1,22 +1,25 @@
 <?php
 
-/**
- * Get filter value for 'from date'.
- */
-function filterDateFrom() {
-    return stringDateToObject($_GET["from_date"]);
-}
+class FilterData {
 
-/**
- * Get filter value for 'to date'.
- */
-function filterDateTo() {
-    return stringDateToObject($_GET["to_date"]);
-}
+    /**
+    * Get filter value for 'from date'.
+    */
+    static function filterDateFrom() {
+        return stringDateToObject($_GET["from_date"]);
+    }
 
-/**
- * Convert string date to DateTime object.
- */
-function stringDateToObject($date) {
-    return DateTime::createFromFormat("! d/m/Y");
+    /**
+    * Get filter value for 'to date'.
+    */
+    static function filterDateTo() {
+        return stringDateToObject($_GET["to_date"]);
+    }
+
+    /**
+    * Convert string date to DateTime object.
+    */
+    static function stringDateToObject($date) {
+        return DateTime::createFromFormat("! d/m/Y");
+    }
 }
