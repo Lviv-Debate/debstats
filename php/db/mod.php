a59bca65737ee8919c0d392347659eefc46ee44b
Maxym Naumchyk <max.naumch@gmail.com>
2017-08-08 15:16:04 +0300

    New fns to get strings 'date from' and 'date to' + fn to convert string date into DateTime object

--- php/db/mod.php
+++ php/db/mod.php
@@ -1,1 +1,22 @@
 <?php
+
+/**
+ * Get filter value for 'from date'.
+ */
+function filterDateFrom() {
+    return stringDateToObject($_GET["from_date"]);
+}
+
+/**
+ * Get filter value for 'to date'.
+ */
+function filterDateTo() {
+    return stringDateToObject($_GET["to_date"]);
+}
+
+/**
+ * Convert string date to DateTime object.
+ */
+function stringDateToObject($date) {
+    return DateTime::createFromFormat("! d/m/Y");
+}
